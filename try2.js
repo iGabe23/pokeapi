e4df75bcdc4c684009b37dfb43f3b16b2f2aa4bb
Gabriel Martinez <2003gabrielmh@gmail.com>
2023-12-08 20:25:17 -0500

    WIP

--- try2.js
+++ try2.js
@@ -1,19 +1,23 @@
+document.addEventListener("DOMContentLoaded", getPokemon);
+
 // Funcion para traer todo del API
-async function getPokemon(id) {
-  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-  const rest = await fetch(url + id);
-  const pokemon = await rest.json();
-  pokemonCard(pokemon);
+async function getPokemon() {
+  const url = `https://pokeapi.co/api/v2/pokemon/`;
+  const res = await fetch(url);
+  const pokemon = await res.json();
+  console.log(pokemon);
 }
 
 // Funcion con ciclo encargada de iterar los datos de los pokemon
+
 async function drawPokemon() {
-  for (let i = 1; i <= 20; i++) {
+  for (let i = 0; i <= 2; i++) {
     await getPokemon(i);
   }
 }
 
 // Funcion que pinta las cartas de cada pokemon en el HTML
+
 function pokemonCard(pokemon) {
   const card = document.createElement("div");
   card.classList.add("pokemon");
